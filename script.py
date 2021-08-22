0c6e3bb59482cbb4e00969a551014f9e31c2299a
rafaelcostaleite <rafaelcostaleite@gmail.com>
2021-08-22 20:03:03 -0300

    primeiro commit

---
+++ script.py
@@ -0,0 +1,19 @@
+#pip install cx_Oracle --upgrade
+
+import pandas as pd
+import cx_Oracle
+import sys
+import logging
+
+#cx_Oracle.init_oracle_client(lib_dir=config['oracle']['lib_dir'])
+
+connection = cx_Oracle.connect(
+    user="totvs",
+    password="totvs",
+    dsn="10.3.0.105:1245/JASPE")
+
+df_ora = pd.read_sql("select A1_COD from SA1000 WHERE A1_COD < '0001000' ", con=connection)
+
+connection.close()
+
+print(df_ora.info())
